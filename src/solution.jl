b788898134db51adcd9df600880d3d2b6777cb64
ajnebro <ajnebro@users.noreply.github.com>
2022-09-03 11:32:41 +0200

    Refactor struct ContinuousSolution

--- src/solution.jl
+++ src/solution.jl
@@ -6,6 +6,7 @@ abstract type Solution end
 mutable struct ContinuousSolution{T <: Number} <: Solution 
     variables::Array{T}
     objectives::Array{Float64}
+    constraints::Array{Float64}
     attributes::Dict
     bounds::Array{Bounds{T}}
 end
