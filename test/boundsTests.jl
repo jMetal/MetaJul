e0acc6a1f38d4690a3e981779268960be981080c
ajnebro <ajnebro@users.noreply.github.com>
2022-09-16 17:04:35 +0200

    Refactor tests for struct Bounds

---
+++ test/boundsTests.jl
@@ -0,0 +1,59 @@
+# Test cases for struct Bounds
+
+function createBoundsAssignTheRightLowerBoundValue() 
+  return Bounds{Real}(4.0, 6.0).lowerBound == 4.0
+end
+
+function createBoundsAssignTheRightUpperBoundValue() 
+    return Bounds{Int}(4, 6).upperBound == 6
+end
+
+function createBoundsWithLowerBoundHigherThanTheLowerBoundRaisesAndException() 
+    return Bounds{Float64}(5.0, 3.0)
+end
+
+function restrictReturnTheValueIfTheValueIsWithinTheBounds()
+    return restrict(4, Bounds{Int64}(1, 5)) == 4
+end
+
+function restrictReturnTheLowerBoundIsTheValueIsTheLowerBound()
+    return restrict(1, Bounds{Int64}(1, 5)) == 1
+end
+
+function restrictReturnTheLowerBoundIsTheValueIsTheUpperBound()
+    return restrict(5.0, Bounds{Float32}(1.0, 5.0)) == 5.0
+end
+
+function restrictReturnTheLowerBoundIsTheValueIsLowerThanTheLowerBound()
+    return restrict(1.0, Bounds{Float64}(1.2, 5.9)) == 1.2
+end
+
+function restrictReturnTheUpperBoundIsTheValueIsHigherThanTheUpperBound()
+    return restrict(10.0, Bounds{Float64}(1.2, 5.9)) == 5.9
+end
+
+function valueIsInBoundsReturnsFalse()
+    return valueIsWithinBounds(10.0, Bounds{Float64}(1.2, 5.9)) == false
+end
+
+function valueIsInBoundsReturnsTrue()
+    return valueIsWithinBounds(3.0, Bounds{Float64}(1.2, 5.9)) == true
+end
+
+
+@testset "Bounds tests" begin
+    @test createBoundsAssignTheRightLowerBoundValue()
+    @test createBoundsAssignTheRightUpperBoundValue()
+
+    @test_throws "The lower bound 5.0 is higher than the upper bound 3.0" createBoundsWithLowerBoundHigherThanTheLowerBoundRaisesAndException()
+
+    @test restrictReturnTheValueIfTheValueIsWithinTheBounds()
+    @test restrictReturnTheLowerBoundIsTheValueIsTheLowerBound()
+    @test restrictReturnTheLowerBoundIsTheValueIsTheUpperBound()
+
+    @test restrictReturnTheLowerBoundIsTheValueIsLowerThanTheLowerBound()
+    @test restrictReturnTheLowerBoundIsTheValueIsLowerThanTheLowerBound()
+
+    @test valueIsInBoundsReturnsFalse()
+    @test valueIsInBoundsReturnsTrue()
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,21 +4,7 @@ include("../src/problem.jl")
 
 using Test
 
-
-@testset "Bounds tests" begin
-    @test restrict(4, Bounds{Int64}(1, 5)) == 4
-    @test restrict(1.0, Bounds{Float64}(1.2, 5.9)) == 1.2
-    @test restrict(10.0, Bounds{Float64}(1.2, 5.9)) == 5.9
-
-    @test valueIsWithinBounds(10.0, Bounds{Float64}(1.2, 5.9)) == false
-    @test valueIsWithinBounds(3.0, Bounds{Float64}(1.2, 5.9)) == true
-
-
-    @test Bounds{Float64}(4.0, 6.0).lowerBound == 4.0
-    @test Bounds{Int}(4, 6).upperBound == 6
-
-    @test_throws "The lower bound 5.0 is higher than the upper bound 3.0" Bounds{Float64}(5.0, 3.0)
-end
+include("boundsTests.jl")
 
 @testset "createBounds() tests" begin
     @test length(createBounds([1, 2, 3], [4, 5, 6])) == 3
