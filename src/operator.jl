740f159a4d69aa35e605d9c82c76a18ab75c010a
ajnebro <ajnebro@users.noreply.github.com>
2022-09-16 17:25:00 +0200

    Add unit tests

--- src/operator.jl
+++ src/operator.jl
@@ -1,11 +1,6 @@
 
 include("solution.jl")
 
-struct Operator
-    execute::Function
-end
-
-
 function uniformMutation(x::Array{T}, probability::T, perturbation::T)::Array{T} where {T <: Real}
   if rand() < probability
     for i in 1:length(x)
