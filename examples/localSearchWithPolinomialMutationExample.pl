954e5e6bbb225d0a126cc1d2959eae334dffa3b5
ajnebro <ajnebro@users.noreply.github.com>
2022-09-27 13:23:59 +0200

    Minor changes

--- examples/localSearchWithPolinomialMutationExample.pl
+++ examples/localSearchWithPolinomialMutationExample.pl
@@ -10,7 +10,7 @@ problem = sphereProblem(10)
 solution = createSolution(problem)
 solution = evaluate(solution, problem)
 
-foundSolution =  localSearch(solution, problem, 10000, polynomialMutationOperator, (probability=0.1, distributionIndex=20.0, bounds=problem.bounds))
+foundSolution = localSearch(solution, problem, 10000, polynomialMutationOperator, (probability=0.1, distributionIndex=20.0, bounds=problem.bounds))
 println("Local search result: ", foundSolution)
 println("Fitness of the starting solution: ", solution.objectives[1])
 println("Fitness of the found solution: ", foundSolution.objectives[1])
