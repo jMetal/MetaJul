13860833118aaa3d84708a249cf3ccc75874f93b
ajnebro <ajnebro@users.noreply.github.com>
2022-09-27 17:41:21 +0200

    Rename files

--- src/solution.jl
+++ src/solution.jl
@@ -24,3 +24,37 @@ end
 function Base.isequal(solution1::ContinuousSolution, solution2::ContinuousSolution)::Bool
     return Base.isequal(solution1.variables, solution2.variables)
 end
+
+###########################################################
+struct BitVector 
+    bits::Vector{Bool}
+end
+
+function initBitVector(zeroAndOneString::String) ::  BitVector
+    bits::Vector{Bool}
+    for char in zeroAndOneString
+        if (char == '0')
+            bits.add(False)
+        else
+            bits.add(True)
+    end
+
+    return BitVector(bits)
+end
+
+mutable struct BinarySolution <: Solution
+    variables::BitVector
+    objectives::Array{Real}
+    constraints::Array{Real}
+    attributes::Dict
+end
+
+function copySolution(solution::BinarySolution{})::BinarySolution{}
+    return ContinuousSolution{}(
+        deepcopy(solution.variables),
+        copy(solution.objectives),
+        copy(solution.constraints),
+        deepcopy(solution.attributes),
+    )
+end
+
