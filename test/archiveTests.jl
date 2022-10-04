70b507783f0f25888acd635becffc81e96909450
ajnebro <ajnebro@users.noreply.github.com>
2022-10-04 20:25:46 +0200

    Refactor test cases

--- test/archiveTests.jl
+++ test/archiveTests.jl
@@ -2,8 +2,14 @@ using Test
 
 include("../src/archive.jl")
 
+# Utility function
+function createContinuousSolution(numberOfObjectives::Int)::ContinuousSolution{Float64}
+  objectives = [_ for _ in range(1, numberOfObjectives)]
+  return ContinuousSolution{Float64}([1.0], objectives, [], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(1.0, 2.0)])
+end
+
 function addASolutionToAnEmtpyArchiveMakesItsLengthToBeOne()
-  solution = ContinuousSolution{Float64}([1.0, 2.0], [1.0, 2.0], [0], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(2, 3)])
+  solution = createContinuousSolution(3)
 
   emptyArchive = NonDominatedArchive{ContinuousSolution{Float64}}([])
   add(emptyArchive, solution)
@@ -12,7 +18,7 @@ function addASolutionToAnEmtpyArchiveMakesItsLengthToBeOne()
 end
 
 function addASolutionToAnEmtpyArchiveMakesItNonEmpty()
-  solution = ContinuousSolution{Float64}([1.0, 2.0], [1.0, 2.0], [0], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(2, 3)])
+  solution = createContinuousSolution(3)
 
   emptyArchive = NonDominatedArchive{ContinuousSolution{Float64}}([])
   add(emptyArchive, solution)
@@ -21,7 +27,7 @@ function addASolutionToAnEmtpyArchiveMakesItNonEmpty()
 end
 
 function addASolutionToAnEmtpyArchiveMakesTheArchiveToContainIt()
-  solution = ContinuousSolution{Float64}([1.0, 2.0], [1.0, 2.0], [0], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(2, 3)])
+  solution = createContinuousSolution(3)
 
   emptyArchive = NonDominatedArchive{ContinuousSolution{Float64}}([])
   add(emptyArchive, solution)
@@ -42,18 +48,26 @@ end
 
 #########################################################################
 
-solution = ContinuousSolution{Float64}([1.0, 2.0], [1.0, 2.0], [0], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(2, 3)])
-nonDominatedSolution = ContinuousSolution{Float64}([1.0, 2.0], [2.0, 1.0], [0], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(2, 3)])
-dominatedSolution = ContinuousSolution{Float64}([1.0, 2.0], [1.5, 2.5], [0], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(2, 3)])
-dominatingSolution = ContinuousSolution{Float64}([1.0, 2.0], [0.5, 0.5], [0], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(2, 3)])
-
 function addANonDominatedSolutionToAnArchiveWithASolutionReturnsTrue() 
+  solution = createContinuousSolution(3)
+  solution.objectives = [1.0, 2.0, 3.0]
+
+  nonDominatedSolution = createContinuousSolution(3)
+  nonDominatedSolution.objectives = [1.0, 1.0, 4.0]
+
   archive = NonDominatedArchive([solution])
   
   return add(archive, nonDominatedSolution)
 end
 
+
 function addANonDominatedSolutionToAnArchiveWithASolutionIncreasesItsLengthByOne() 
+  solution = createContinuousSolution(3)
+  solution.objectives = [1.0, 2.0, 3.0]
+
+  nonDominatedSolution = createContinuousSolution(3)
+  nonDominatedSolution.objectives = [1.0, 1.0, 4.0]
+
   archive = NonDominatedArchive([solution])
   add(archive, nonDominatedSolution)
 
@@ -61,6 +75,12 @@ function addANonDominatedSolutionToAnArchiveWithASolutionIncreasesItsLengthByOne
 end
 
 function addADominatedSolutionToAnArchiveWithASolutionReturnFalse() 
+  solution = createContinuousSolution(3)
+  solution.objectives = [1.0, 2.0, 3.0]
+
+  dominatedSolution = createContinuousSolution(3)
+  dominatedSolution.objectives = [2.0, 3.0, 4.0]
+
   archive = NonDominatedArchive([solution])
   add(archive, dominatedSolution)
 
@@ -69,19 +89,38 @@ end
 
 
 function addADominatedSolutionToAnArchiveWithASolutionDoesNotIncreasesItsLength() 
+  solution = createContinuousSolution(3)
+  solution.objectives = [1.0, 2.0, 3.0]
+
+  dominatedSolution = createContinuousSolution(3)
+  dominatedSolution.objectives = [2.0, 3.0, 4.0]
+
   archive = NonDominatedArchive([solution])
   add(archive, dominatedSolution)
 
   return length(archive) == 1
 end
 
+
 function addADominatingSolutionToAnArchiveWithASolutionReturnsTrue() 
+  solution = createContinuousSolution(3)
+  solution.objectives = [1.0, 2.0, 3.0]
+
+  dominatingSolution = createContinuousSolution(3)
+  dominatingSolution.objectives = [0.0, 1.0, 2.0]
+
   archive = NonDominatedArchive([solution])
   
   return add(archive, dominatingSolution)
 end
 
 function addADominatingSolutionToAnArchiveWithASolutionDoesNotIncreasesItsLength() 
+  solution = createContinuousSolution(3)
+  solution.objectives = [1.0, 2.0, 3.0]
+
+  dominatingSolution = createContinuousSolution(3)
+  dominatingSolution.objectives = [0.0, 1.0, 2.0]
+
   archive = NonDominatedArchive([solution])
   add(archive, dominatingSolution)
 
@@ -89,14 +128,19 @@ function addADominatingSolutionToAnArchiveWithASolutionDoesNotIncreasesItsLength
 end
 
 function addADominatingSolutionToAnArchiveWithASolutionRemovesTheExistingOneWhichIsReplacedByTheDominatingOne() 
+  solution = createContinuousSolution(3)
+  solution.objectives = [1.0, 2.0, 3.0]
+
+  dominatingSolution = createContinuousSolution(3)
+  dominatingSolution.objectives = [0.0, 1.0, 2.0]
+
   archive = NonDominatedArchive([solution])
   add(archive, dominatingSolution)
 
   return isequal(dominatingSolution, archive.solutions[1])
 end
 
-
-archiveWithASolution = NonDominatedArchive([solution])
+archiveWithASolution = NonDominatedArchive([createContinuousSolution(5)])
 @testset "Archive with a solution tests" begin    
   @test length(archiveWithASolution) == 1
   @test !isEmpty(archiveWithASolution)
@@ -107,7 +151,9 @@ archiveWithASolution = NonDominatedArchive([solution])
   @test addADominatedSolutionToAnArchiveWithASolutionReturnFalse()
   @test addADominatedSolutionToAnArchiveWithASolutionDoesNotIncreasesItsLength()
   
+  @test addADominatingSolutionToAnArchiveWithASolutionReturnsTrue()
   @test addADominatingSolutionToAnArchiveWithASolutionDoesNotIncreasesItsLength()
   @test addADominatingSolutionToAnArchiveWithASolutionRemovesTheExistingOneWhichIsReplacedByTheDominatingOne()
-  @test addADominatingSolutionToAnArchiveWithASolutionReturnsTrue()
 end
+
+#########################################################################
