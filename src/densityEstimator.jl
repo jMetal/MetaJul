6a01496497d3e8991d62bf6fc3b4257777855480
ajnebro <ajnebro@users.noreply.github.com>
2022-10-05 17:48:37 +0200

    Add files densityEstimator.jl and densityEstimatorTests.jl

---
+++ src/densityEstimator.jl
@@ -0,0 +1,63 @@
+include("core.jl")
+include("solution.jl")
+include("archive.jl")
+
+# Struct and methods to implement the non-dominated ranking sorting method
+
+# abstract type DensityEstimator end
+
+
+"""
+    computeCrowdingDistanceEstimator!(solutions::Vector{T}) where {T <: Solution}
+
+Computes the crowding distance density estimator to the solutions of a list. It is assumed that the list only contains non-dominated solutions.
+"""
+function computeCrowdingDistanceEstimator!(solutions::Vector{T}) where {T <: Solution}
+    @assert length(solutions) > 0 "The solution list is empty"
+    if length(solutions) < 3
+        for solution in solutions
+            solution.attributes["CROWDING_DISTANCE_ATTRIBUTE"] = typemax(Float64)
+        end
+    else
+        println("NUMBER OF OBJECTIVES: ", length(solutions[1].objectives))
+        numberOfObjectives = length(solutions[1].objectives)
+        for solution in solutions
+            solution.attributes["CROWDING_DISTANCE_ATTRIBUTE"] = 0.0
+        end
+
+        for i in range(1, numberOfObjectives)
+            sort!(solutions, by = s -> s.objectives[i])
+
+            minimumObjectiveValue = solutions[1].objectives[i]
+            maximumObjectiveValue = solutions[numberOfObjectives].objectives[i]
+
+            solutions[1].attributes["CROWDING_DISTANCE_ATTRIBUTE"] = typemax(Float64)
+            solutions[length(solutions)].attributes["CROWDING_DISTANCE_ATTRIBUTE"] = typemax(Float64)
+
+            for j in range(2, length(solutions)-1)
+                distance = solutions[j+1].objectives[i] - solutions[j-1].objectives[i]
+                distance = distance / (maximumObjectiveValue - minimumObjectiveValue)
+                distance = distance + solutions[j].attributes["CROWDING_DISTANCE_ATTRIBUTE"]
+                solutions[j].attributes["CROWDING_DISTANCE_ATTRIBUTE"] = distance
+            end
+            
+        end
+    end
+
+    return Nothing
+end
+
+
+"""
+function createContinuousSolution(objectives::Vector{Float64})::ContinuousSolution{Float64}
+    return ContinuousSolution{Float64}([1.0], objectives, [], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(1.0, 2.0)])
+end
+
+solution1 = createContinuousSolution([0.0, 1.0])
+solution2 = createContinuousSolution([1.0, 0.0])
+solution3 = createContinuousSolution([0.5, 0.5])
+solutions = [solution1, solution2, solution3]
+
+computeCrowdingDistanceEstimator!(solutions)
+
+"""
---
+++ test/densityEstimatorTests.jl
@@ -0,0 +1,49 @@
+include("../src/solution.jl")
+include("../src/DensityEstimator.jl")
+
+using Test
+
+# Utility functions
+function createContinuousSolution(objectives::Vector{Float64})::ContinuousSolution{Float64}
+    return ContinuousSolution{Float64}([1.0], objectives, [], Dict(), [Bounds{Float64}(1.0, 2.0), Bounds{Float64}(1.0, 2.0)])
+end
+
+function computingTheCrowdingDistanceRaisesAnExceptionIfTheSolutionListIsEmpty()
+    computeCrowdingDistanceEstimator!(Vector{ContinuousSolution{Float64}}(undef, 0))
+end
+
+function computingTheCrowdingDistanceOnAListWithASolutionAssignsTheMaxValueToTheSolution()
+    solutions = [createContinuousSolution([1.0, 2.0])]
+    computeCrowdingDistanceEstimator!(solutions)
+
+    return solutions[1].attributes["CROWDING_DISTANCE_ATTRIBUTE"] == typemax(Float64)
+end
+
+function computingTheCrowdingDistanceOnAListWithTwoSolutionAssignsTheMaxValueToThem()
+    solutions = [createContinuousSolution([1.0, 2.0]), createContinuousSolution([2.0, 1.0])]
+    computeCrowdingDistanceEstimator!(solutions)
+
+    return solutions[1].attributes["CROWDING_DISTANCE_ATTRIBUTE"] == typemax(Float64)
+    return solutions[2].attributes["CROWDING_DISTANCE_ATTRIBUTE"] == typemax(Float64)
+end
+
+function computingTheCrowdingDistanceOnAListWithThreeBiObjectiveSolutionAssignsTheRightValues()
+    solution1 = createContinuousSolution([0.0, 1.0])
+    solution2 = createContinuousSolution([1.0, 0.0])
+    solution3 = createContinuousSolution([0.5, 0.5])
+    solutions = [solution1, solution2, solution3]
+
+    computeCrowdingDistanceEstimator!(solutions)
+
+    return solutions[1].attributes["CROWDING_DISTANCE_ATTRIBUTE"] == typemax(Float64)
+    return solutions[2].attributes["CROWDING_DISTANCE_ATTRIBUTE"] == 2.0
+    return solutions[3].attributes["CROWDING_DISTANCE_ATTRIBUTE"] == typemax(Float64)
+end
+
+@testset "Crowding distance estimator test cases" begin    
+    @test_throws "The solution list is empty" computingTheCrowdingDistanceRaisesAnExceptionIfTheSolutionListIsEmpty()
+
+    @test computingTheCrowdingDistanceOnAListWithASolutionAssignsTheMaxValueToTheSolution()
+    @test computingTheCrowdingDistanceOnAListWithTwoSolutionAssignsTheMaxValueToThem()
+    @test computingTheCrowdingDistanceOnAListWithThreeBiObjectiveSolutionAssignsTheRightValues()
+end
