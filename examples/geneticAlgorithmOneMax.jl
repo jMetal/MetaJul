c0fd2649a85a3594cf39f96e50a542b25233b0f8
ajnebro <ajnebro@users.noreply.github.com>
2022-10-12 12:31:02 +0200

    Rename file

---
+++ examples/geneticAlgorithmOneMax.jl
@@ -0,0 +1,39 @@
+include("../src/bounds.jl")
+include("../src/solution.jl")
+include("../src/operator.jl")
+include("../src/binaryProblem.jl")
+include("../src/algorithm.jl")
+include("../src/component.jl")
+
+using Dates
+
+# Genetic algorithm example applied to problem OneMax
+problem = oneMax(1024)
+
+solver::GeneticAlgorithm = GeneticAlgorithm()
+solver.problem = problem
+solver.numberOfEvaluations = 40000
+solver.populationSize = 100
+solver.offspringPopulationSize = 100
+solver.mutation = bitFlipMutation
+solver.mutationParameters = (probability=1.0/numberOfVariables(problem),)
+solver.crossover = singlePointCrossover
+solver.crossoverParameters = (probability = 1.0,)
+
+solver.termination = terminationByEvaluations
+
+solver.selection = binaryTournamentSelection
+solver.selectionParameters = (matingPoolSize = 100, comparator = objectiveComparator)
+
+solver.variation = crossoverAndMutationVariation
+
+startingTime = Dates.now()
+optimize(solver)
+endTime = Dates.now()
+
+foundSolutions = solver.foundSolutions
+
+println("GA result: ", length(foundSolutions))
+println("Result: ", foundSolutions[1].objectives)
+println("Result: ", foundSolutions[1].variables)
+println("Computing time: ", (endTime - startingTime))
