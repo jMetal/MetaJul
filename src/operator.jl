b30861c9caa5f95907139aa1f10a5dbaf73f47e4
ajnebro <ajnebro@users.noreply.github.com>
2022-10-18 16:07:05 +0200

    Add simulated binary crossover (SBX)

--- src/operator.jl
+++ src/operator.jl
@@ -22,13 +22,13 @@ function bitFlipMutation(solution::BinarySolution, parameters)::BinarySolution
 end
 
 
-function uniformMutation(x::Vector{T}, parameters)::Vector{T} where {T <: Real}
+function uniformMutation(x::Vector{T}, parameters)::Vector{T} where {T<:Real}
   probability::Real = parameters.probability
   perturbation::Real = parameters.perturbation
   bounds = parameters.bounds
   for i in 1:length(x)
     if rand() < probability
-        x[i] += (rand() - 0.5) * perturbation
+      x[i] += (rand() - 0.5) * perturbation
     end
   end
 
@@ -42,36 +42,36 @@ function uniformMutation(solution::ContinuousSolution, parameters)::ContinuousSo
 end
 
 
-function polynomialMutation(x::Vector{T}, parameters)::Vector{T} where {T <: Real}
+function polynomialMutation(x::Vector{T}, parameters)::Vector{T} where {T<:Real}
   probability::Real = parameters.probability
   distributionIndex::Real = parameters.distributionIndex
   bounds = parameters.bounds
 
   for i in 1:length(x)
     if rand() <= probability
-        y = x[i]
-        yl = bounds[i].lowerBound
-        yu = bounds[i].upperBound
-        if (yl == yu)
-          y = yl
+      y = x[i]
+      yl = bounds[i].lowerBound
+      yu = bounds[i].upperBound
+      if (yl == yu)
+        y = yl
+      else
+        delta1 = (y - yl) / (yu - yl)
+        delta2 = (yu - y) / (yu - yl)
+        rnd = rand()
+        mutPow = 1.0 / (distributionIndex + 1.0)
+        if (rnd <= 0.5)
+          xy = 1.0 - delta1
+          val = 2.0 * rnd + (1.0 - 2.0 * rnd) * (xy^(distributionIndex + 1.0))
+          deltaq = (val^mutPow) - 1.0
         else
-          delta1 = (y - yl) / (yu - yl);
-          delta2 = (yu - y) / (yu - yl);
-          rnd = rand()
-          mutPow = 1.0 / (distributionIndex + 1.0)
-          if (rnd <= 0.5) 
-            xy = 1.0 - delta1
-            val = 2.0 * rnd + (1.0 - 2.0 * rnd) * (xy^ (distributionIndex + 1.0))
-            deltaq = (val ^ mutPow) - 1.0
-          else 
-            xy = 1.0 - delta2
-            val = 2.0 * (1.0 - rnd) + 2.0 * (rnd - 0.5) * (xy ^ (distributionIndex + 1.0))
-            deltaq = 1.0 - (val ^ mutPow)
-          end
-          y = y + deltaq * (yu - yl)
+          xy = 1.0 - delta2
+          val = 2.0 * (1.0 - rnd) + 2.0 * (rnd - 0.5) * (xy^(distributionIndex + 1.0))
+          deltaq = 1.0 - (val^mutPow)
         end
-        x[i] = y
+        y = y + deltaq * (yu - yl)
       end
+      x[i] = y
+    end
   end
   x = randomRestrict(x, bounds)
   return x
@@ -92,7 +92,7 @@ function blxAlphaCrossover(parent1::ContinuousSolution, parent2::ContinuousSolut
   child2 = copySolution(parent2)
 
   if rand() < probability
-    for i in range(1,length(parent1.variables))
+    for i in range(1, length(parent1.variables))
       minValue = min(parent1.variables[i], parent2.variables[i])
       maxValue = max(parent1.variables[i], parent2.variables[i])
       range = maxValue - minValue
@@ -106,9 +106,87 @@ function blxAlphaCrossover(parent1::ContinuousSolution, parent2::ContinuousSolut
       child2.variables[i] = minRange + random * (maxRange - minRange)
     end
   end
-  
+
   child1.variables = restrict(child1.variables, bounds)
   child2.variables = restrict(child2.variables, bounds)
+
+  return [child1, child2]
+end
+
+
+"""
+    sbxCrossover(parent1::ContinuousSolution, parent2::ContinuousSolution, parameters::NamedTuple)::Vector{ContinuousSolution}
+
+Simulated binary crossover operator
+"""
+function sbxCrossover(parent1::ContinuousSolution, parent2::ContinuousSolution, parameters::NamedTuple)::Vector{ContinuousSolution}
+  EPSILON = 1.0e-14
+  probability::Real = parameters.probability
+  distributionIndex::Real = parameters.distributionIndex
+  bounds = parameters.bounds
+
+  child1 = copySolution(parent1)
+  child2 = copySolution(parent2)
+
+  if rand() <= probability
+    for i in range(1, length(parent1.variables))
+      x1 = parent1.variables[i]
+      x2 = parent2.variables[i]
+
+      y1 = 0.0
+      y2 = 0.0
+      if (rand() <= 0.5)
+        if (abs(x1 - x2) > EPSILON)
+          if (x1 < x2)
+            y1 = x1
+            y2 = x2
+          else
+            y1 = x2
+            y2 = x1
+          end
+
+          random = rand()
+          beta = 1.0 + (2.0 * (y1 - bounds[i].lowerBound) / (y2 - y1))
+          alpha = 2.0 - ^(beta, -(distributionIndex + 1.0))
+        
+          betaq = 0.0
+          if random <= (1.0 / alpha)
+            betaq = ^(random * alpha, (1.0 / distributionIndex + 1.0))
+          else
+            betaq = ^(1.0 / (2.0 - random * alpha), 1.0 / (distributionIndex + 1.0))
+          end
+          c1 = 0.5 * (y1 + y2 - betaq * (y2 - y1))
+
+          beta = 1.0 + (2.0 * (bounds[i].upperBound - y2) / (y2 - y1))
+          alpha = 2.0 - ^(beta, -(distributionIndex + 1.0))
+
+          if random <= (1.0 / alpha)
+            betaq = ^(random * alpha, (1.0 / distributionIndex + 1.0))
+          else
+            betaq = ^(1.0 / (2.0 - random * alpha), 1.0 / (distributionIndex + 1.0))
+          end
+          c2 = 0.5 * (y1 + y2 - betaq * (y2 - y1))
+
+          c1 = restrict(c1, bounds[i])
+          c2 = restrict(c2, bounds[i])
+
+          if rand() <= 0.5
+            child1.variables[i] = c2
+            child2.variables[i] = c1
+          else
+            child1.variables[i] = c1
+            child2.variables[i] = c2
+          end
+        else
+          child1.variables[i] = x1
+          child2.variables[i] = x2
+        end
+      else
+        child1.variables[i] = x2
+        child2.variables[i] = x1
+      end
+    end
+  end
 
   return [child1, child2]
 end
@@ -126,7 +204,7 @@ function singlePointCrossover(parent1::BinarySolution, parent2::BinarySolution, 
   if rand() < parameters.probability
     cuttingPoint = rand(1:length(x))
 
-    tmp = x.bits[cuttingPoint:end] 
+    tmp = x.bits[cuttingPoint:end]
     x.bits[cuttingPoint:end] = y.bits[cuttingPoint:end]
     y.bits[cuttingPoint:end] = tmp
   end
@@ -139,7 +217,7 @@ end
 
 
 # Selection operators
-function randomSelection(x::Vector, parameters = [])
+function randomSelection(x::Vector, parameters=[])
   return x[rand(1:length(x))]
 end
 
@@ -148,7 +226,7 @@ function binaryTournamentSelection(x::Vector{Solution}, parameters::NamedTuple)
   index1 = rand(1:length(x))
   index2 = rand(1:length(x))
 
-  if comparator(x[index1], x[index2]) < 0 
+  if comparator(x[index1], x[index2]) < 0
     result = x[index1]
   else
     result = x[index2]
@@ -165,7 +243,7 @@ end
 TBW
 """
 function muPlusLambdaReplacement(x::Vector, y::Vector, comparator::Function=isless)
-  jointVector = vcat(x,y)
+  jointVector = vcat(x, y)
   sort!(jointVector, lt=comparator)
   return jointVector[1:length(x)]
 end
@@ -176,7 +254,7 @@ end
 TBW
 """
 function muCommaLambdaReplacement(x::Vector, y::Vector, comparator::Function=isless)
-  @assert length(x) >= length(y) "The length of the x vector is lower than the length of the y vector" 
+  @assert length(x) >= length(y) "The length of the x vector is lower than the length of the y vector"
 
   resultVector = Vector(y)
   sort!(resultVector, lt=comparator)
