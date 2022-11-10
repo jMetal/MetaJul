737546e8862495d0a20737802fd246f4ad9cf4b2
ajnebro <ajnebro@users.noreply.github.com>
2022-11-10 17:58:36 +0100

    Working on constraint handling

--- src/comparator.jl
+++ src/comparator.jl
@@ -1,4 +1,5 @@
 include("core.jl")
+include("constraintHandling.jl")
 
 """
     compareElementAt(x, y)
@@ -7,13 +8,13 @@ Compare the values in the same position (`index`) of two numeric vectors `x` and
 +1 depending, respectively, on the conditions `x[index] == y[index]`, `x[index] < y[index]` or `x[index] > y[index]`
 
 """
-function compareElementAt(x::Vector{T}, y::Vector{T}, index::Int=1)::Int where {T <: Number}
+function compareElementAt(x::Vector{T}, y::Vector{T}, index::Int=1)::Int where {T<:Number}
   @assert length(x) == length(y) "The vectors have a different length"
   @assert index in range(1, length(x)) "The index is out of range"
 
-  result = 0 ;
+  result = 0
   if x[index] < y[index]
-    result = -1 ;
+    result = -1
   elseif x[index] > y[index]
     result = 1
   end
@@ -27,48 +28,61 @@ end
 Compare two numerics vectors `x` and `y` according to the dominance relationship. The result is 0 if both vectors are non-dominated, -1 if vector `x` dominates vector `y`, and +1 if vector `x` is dominated by vector `y`. The dominance comparison assumes minimization, i.e., the lower the compared values, the better.
 
 """
-function compareForDominance(x::Vector{T}, y::Vector{T})::Int where {T <: Number}
-    @assert length(x) == length(y) "The vectors have a different length"
-  
-    """
-    x==y && return 0
-  
-    all(t->(t[1]≤t[2]), zip(x, y)) && return -1
-    all(t->(t[1]≤t[2]), zip(y, x)) && return  1
-    return 0
-  
-    """
-    bestIsSolution1 = 0
-    bestIsSolution2 = 0
-    result = 0 
-  
-    for i in 1:length(x)
-      if x[i] != y[i]
-        if x[i] < y[i]
-          y[i]
-          bestIsSolution1 = 1
-        end
-        if x[i] > y[i]
-          bestIsSolution2 = 1
-        end
+function compareForDominance(x::Vector{T}, y::Vector{T})::Int where {T<:Number}
+  @assert length(x) == length(y) "The vectors have a different length"
+
+  """
+  x==y && return 0
+
+  all(t->(t[1]≤t[2]), zip(x, y)) && return -1
+  all(t->(t[1]≤t[2]), zip(y, x)) && return  1
+  return 0
+
+  """
+  bestIsSolution1 = 0
+  bestIsSolution2 = 0
+  result = 0
+
+  for i in 1:length(x)
+    if x[i] != y[i]
+      if x[i] < y[i]
+        y[i]
+        bestIsSolution1 = 1
+      end
+      if x[i] > y[i]
+        bestIsSolution2 = 1
       end
     end
-  
-    if bestIsSolution1 > bestIsSolution2
-      result = -1
-    elseif bestIsSolution1 < bestIsSolution2
-      result = 1
-    end
-  
-    return result
-  end
-  
-  function compareForDominance(solution1::Solution, solution2::Solution)::Int
-    return compareForDominance(solution1.objectives, solution2.objectives)
-  end
-  
-  function compareIthObjective(solution1::Solution, solution2::Solution, index::Int = 1)::Int
-    return  compareElementAt(solution1.objectives, solution2.objectives, index)
   end
 
+  if bestIsSolution1 > bestIsSolution2
+    result = -1
+  elseif bestIsSolution1 < bestIsSolution2
+    result = 1
+  end
+
+  return result
+end
+
+function compareForDominance(solution1::Solution, solution2::Solution)::Int
+  return compareForDominance(solution1.objectives, solution2.objectives)
+end
+
+function compareIthObjective(solution1::Solution, solution2::Solution, index::Int=1)::Int
+  return compareElementAt(solution1.objectives, solution2.objectives, index)
+end
+
+function compareForOverallConstraintViolationDegree((solution1::Solution, solution2::Solution)::Int)
+  constraintViolationSolution1 = overallConstraintViolationDegree(solution1)
+  constraintViolationSolution2 = overallConstraintViolationDegree(solution2)
+
+  result = 0 
+  if constraintViolationSolution1 < constraintViolationSolution2
+    result = -1
+  elseif constraintViolationSolution1 > constraintViolationSolution2
+    result = 1
+  end
+
+  return result
+end
 
---
+++ src/constraintHandling.jl
@@ -0,0 +1,34 @@
+
+include("continuousProblem.jl")
+
+function overallConstraintViolationDegree(solution::S) where {S <:Solution}
+  return sum(filter(x -> x < 0.0, solution.constraints))
+end
+
+function numberOfViolatedConstraints(solution::S) where {S <:Solution}
+  return length(filter(x -> x < 0.0, solution.constraints))
+end
+
+
+
+#### Constrained problem
+function constrExProblem()
+  problem = ContinuousProblem{Float64}("ConstrEx")
+
+  addVariable(problem, Bounds{Float64}(0.1, 1.0))
+  addVariable(problem, Bounds{Float64}(0.0, 5.0))
+
+  f1 = x -> x[1]
+  f2 = x -> (1.0 + x[2]/x[1])
+
+  addObjective(problem, f1)
+  addObjective(problem, f2)
+
+  c1 = x -> x[2] + 9 * x[1] - 6.0
+  c2 = x -> -x[2] + 9 * x[1] - 1.0
+
+  addConstraint(problem, c1)
+  addConstraint(problem, c2)
+
+  return problem
+end
