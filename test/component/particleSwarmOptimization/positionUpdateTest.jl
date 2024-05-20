dfa3fcf1fb5b6ad870cd7e69236421afa8053910
ajnebro <ajnebro@users.noreply.github.com>
2024-05-20 16:57:02 +0200

    Fix a bug in a test

--- test/component/particleSwarmOptimization/positionUpdateTest.jl
+++ test/component/particleSwarmOptimization/positionUpdateTest.jl
@@ -15,7 +15,7 @@ function positionUpdateWorksProperlyWithASingleParticle()
     velocityChangeWhenLowerLimitIsReached = -1.0
     velocityChangeWhenUpperLimitIsReached = 0.25
     positionBounds = [Bounds{Float64}(-10.0, 10.0), Bounds{Float64}(-15.0, 15.0)]
-    speed = [[0.5, -0.5]]
+    speed = [0.5 -0.5]
     variables = [2.0, 4.0]
 
     particle = ContinuousSolution{Float64}(copy(variables), [1.5, 2.5], [], Dict(), positionBounds)
@@ -25,14 +25,14 @@ function positionUpdateWorksProperlyWithASingleParticle()
 
     update(positionUpdate, swarm, speed)
 
-    return speed[1] .+ variables == particle.variables
+    return [2.5, 3.5] == particle.variables
 end
 
 function positionUpdateWorksProperlyWithASingleParticleWithPositionsOutOfBounds()
     velocityChangeWhenLowerLimitIsReached = -1.0
     velocityChangeWhenUpperLimitIsReached = 1
     positionBounds = [Bounds{Float64}(-10.0, 10.0), Bounds{Float64}(-15.0, 15.0)]
-    speed = [[-1.0, 1.0]]
+    speed = [-1.0 1.0]
     variables = [-9.5, 14.5]
 
     particle = ContinuousSolution{Float64}(copy(variables), [1.5, 2.5], [], Dict(), positionBounds)
