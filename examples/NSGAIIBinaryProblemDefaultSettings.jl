46a4269c8856e3277ec58be1eab74cf163fc3c8f
ajnebro <ajnebro@users.noreply.github.com>
2024-06-07 23:04:10 +0200

    Redefine binary examples

---
+++ examples/NSGAIIBinaryProblemDefaultSettings.jl
@@ -0,0 +1,32 @@
+using MetaJul
+
+using Dates
+
+# NSGA-II algorithm example configured from the NSGA-II template
+function main()
+    problem = oneZeroMax(512)
+
+    solver::NSGAII = NSGAII()
+    solver.problem = problem
+
+    solver.mutation = BitFlipMutation(1.0 / numberOfVariables(problem))
+    solver.crossover = SinglePointCrossover(1.0)
+
+    startingTime = Dates.now()
+    optimize(solver)
+    endTime = Dates.now()
+
+    front = foundSolutions(solver)
+
+    objectivesFileName = "FUN.csv"
+    variablesFileName = "VAR.csv"
+
+    println("Algorithm: ", name(solver))
+
+    println("Objectives stored in file ", objectivesFileName)
+    printObjectivesToCSVFile(objectivesFileName, front)
+
+    println("Variables stored in file ", variablesFileName)
+    printVariablesToCSVFile(variablesFileName, front)
+    println("Computing time: ", (endTime - startingTime))
+end
